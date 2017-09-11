ebc6a72a104077e1b45689cc7248ebc01553d3bd
Zhouyu Meng <misskanagi@gmail.com>
2017-09-12 01:26:23 +0800

    minor revise

--- conf.lua
+++ conf.lua
@@ -4,9 +4,9 @@ function love.conf(t)
 	t.console = false
 	t.window.msaa = 2
 	t.window.vsync = true
-	t.window.height = 800
-	t.window.width = 1280
-	t.window.fullscreen = true
+	t.window.height = 600
+	t.window.width = 800
+	t.window.fullscreen = false
   t.modules.image = true
   t.modules.timer = true
 end
