39170e84bcd85efe50deb485b1092190f8bd46e9
dr <3150127678@qq.com>
2017-09-23 02:18:57 +0800

    aaa

--- boom/systems/HUD/HUDMyInfo.lua
+++ boom/systems/HUD/HUDMyInfo.lua
@@ -4,10 +4,11 @@ local HUD_canvas = require "boom.systems.HUD.HUD_canvas"
 local group_hudmyinfo = 101
 local HUDMyInfo = class("HUDMyInfo", System)
 local img = love.graphics.newImage("/assets/sign_bullet.png")
-
+local img_heart = love.graphics.newImage("/assets/heart.png")
 local pb_hp = nil  --血条
 local lbl_shell = nil --导弹仓
 local lbl_life = nil --生命数
+local grid_life = nil
 
 function HUDMyInfo:draw()
   print("HUDMyInfo draw()")
@@ -22,12 +23,19 @@ function HUDMyInfo:draw()
     gooi.setStyle(style)
     gooi.desktopMode()
     gooi.shadow()
-    
-    pb_hp = gooi.newBar({x = 10, y = 10, w = 300, h = 10, value = 1, group = group_hudmyinfo})
+    --创建血条
+    pb_hp = gooi.newBar({x = 10, y = 10, w = 320, h = 10, value = 1, group = group_hudmyinfo})
     :bg({255, 255, 255, 60})
     :fg({0, 255, 0, 150})
+    --创建全队生命示意图
+    grid_life = gooi.newPanel({x = 10, y = 30, w = 320, h = 32, layout = "grid 1x10", group = group_hudmyinfo})
+    for i = 1, 10 do
+      local lbl_item = gooi.newLabel({text = "", group = group_hudmyinfo}):setIcon(img_heart)
+      grid_life:add(lbl_item, "1,"..i)
+    end
     
-    lbl_shell = gooi.newLabel({x = 10, y = 30, text = "", group = group_hudmyinfo}):left()
+    
+    lbl_shell = gooi.newLabel({x = 10, y = 70, text = "", group = group_hudmyinfo}):left()
     
     self.is_init = true
   end
