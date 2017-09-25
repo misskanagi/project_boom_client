1aa0c8380e6e02d1b567061f2cc5bba0b5e62c1b
dr <3150127678@qq.com>
2017-09-25 19:29:23 +0800

    ...

--- boom/systems/HUD/HUDMyInfo.lua
+++ boom/systems/HUD/HUDMyInfo.lua
@@ -5,10 +5,11 @@ local group_hudmyinfo = 101
 local HUDMyInfo = class("HUDMyInfo", System)
 local img = love.graphics.newImage("/assets/backup/sign_bullet.png")
 local img_heart = love.graphics.newImage("/assets/heart.png")
+local img_blank = love.graphics.newImage("/assets/backup/blank_32.png")
 local pb_hp = nil  --血条
 local lbl_shell = nil --导弹仓
-local lbl_life = nil --生命数
 local grid_life = nil
+local lbl_hearts = {} --存放所有的生命爱心控件
 
 function HUDMyInfo:draw()
   --print("HUDMyInfo draw()")
@@ -29,16 +30,18 @@ function HUDMyInfo:draw()
     :fg({0, 255, 0, 150})
     --创建全队生命示意图
     grid_life = gooi.newPanel({x = 10, y = 30, w = 320, h = 32, layout = "grid 1x10", group = group_hudmyinfo})
-    for i = 1, 10 do
-      local lbl_item = gooi.newLabel({text = "", group = group_hudmyinfo}):setIcon(img_heart)
+    --获取全队生命总数
+    local group_life_count = 10
+    for i = 1, group_life_count do
+      local lbl_item = gooi.newLabel({text = "", group = group_hudmyinfo}):setIcon(img_blank)
       grid_life:add(lbl_item, "1,"..i)
+      lbl_hearts[#lbl_hearts+1] = lbl_item
     end
-    
-    
     lbl_shell = gooi.newLabel({x = 10, y = 70, text = "", group = group_hudmyinfo}):left()
-    
     self.is_init = true
   end
+  
+  --常规draw
   local cvs = love.graphics.getCanvas()
   love.graphics.setCanvas(HUD_canvas:getCanvas())
   love.graphics.push()
@@ -53,13 +56,23 @@ function HUDMyInfo:draw()
     local launchable_num = launchable.shell_count
     local launchable_name = launchable.shell_name
     lbl_shell:setIcon(img):setText(launchable_name.." Rest : "..launchable_num)
-    
+    --绘制全队的生命爱心
+    local group_life_count = 8
+    for i = 1, #lbl_hearts do
+      if i <= group_life_count then
+        lbl_hearts[i]:setIcon(img_heart)
+      else 
+        lbl_hearts[i]:setIcon(img_blank)
+      end
+    end
   end
   gooi.draw(group_hudmyinfo)
-  --love.graphics.setColor(255,255,255,255)
-  --love.graphics.rectangle("fill", 0, 0, 200, 200)
   love.graphics.pop()
   love.graphics.setCanvas(cvs)
+end
+
+function HUDMyInfo:update_widgets()
+  
 end
 
 
