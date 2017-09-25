3a9998ab66d730ee3ed13e0fa257bf8d2363be13
Zhouyu Meng <misskanagi@gmail.com>
2017-09-26 00:47:41 +0800

    fuck

--- conf.lua
+++ conf.lua
@@ -2,12 +2,12 @@ function love.conf(t)
 	t.identity = "PROJBOOM"
 	t.title = "PROJBOOM"
 	t.console = false
-	--t.window.msaa = 4
+	t.window.msaa = 4
 	t.window.vsync = true
 	t.window.height = 600
-	t.window.width = 800
-	t.window.fullscreen = false
-	--t.window.fullscreentype = "exclusive"
+	t.window.width = 960
+	t.window.fullscreen = true
+	t.window.fullscreentype = "exclusive"
   t.modules.image = true
   t.modules.timer = true
 end
