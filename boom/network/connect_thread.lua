dde0bd698ad79d6f2e7a1dc82a66ce57d1b15018
dr <3150127678@qq.com>
2017-09-26 23:23:40 +0800

    ...

--- boom/network/connect_thread.lua
+++ boom/network/connect_thread.lua
@@ -6,6 +6,7 @@ local channel_connect = love.thread.getChannel("channel_connect")
 --net:connect("192.168.1.105", 8080)
 --net:connect("172.28.37.19", 8080)
 --netLib.Lua_connect("114.212.83.208", 8080)
-netLib.Lua_connect("192.168.1.105", 8080)
+--netLib.Lua_connect("192.168.1.105", 8080)
+netLib.Lua_connect("192.168.233.181", 8080)
 channel_connect:push(true)
 
