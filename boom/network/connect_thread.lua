2c5b757740afc1d6efc2e5e2833478cb37d0391b
dr <3150127678@qq.com>
2017-09-26 23:08:01 +0800

    ...

--- boom/network/connect_thread.lua
+++ boom/network/connect_thread.lua
@@ -3,8 +3,9 @@ local json = require "cjson"
 
 --获取network单例并连接
 local channel_connect = love.thread.getChannel("channel_connect")
-net:connect("192.168.1.105", 8080)
+--net:connect("192.168.1.105", 8080)
 --net:connect("172.28.37.19", 8080)
 --netLib.Lua_connect("114.212.83.208", 8080)
+netLib.Lua_connect("192.168.1.105", 8080)
 channel_connect:push(true)
 
